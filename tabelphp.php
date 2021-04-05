0cc044ad18d23e04d79bc205769973938601b5b8
briandytriputra <74373398+briandytriputra@users.noreply.github.com>
2021-04-05 18:55:25 +0700

    Create tabelphp.php

---
+++ tabelphp.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Integrasi PHP</title>
+	<link rel="stylesheet" type="text/css" href="tabel.css">
+</head>
+<body>
+<?php
+echo "Nama &nbsp&nbsp: Rizky Ramadhani<br>";
+echo "NIM &nbsp&nbsp&nbsp&nbsp: 191410073<br>";
+echo "Kelas &nbsp&nbsp&nbsp: SI4A<br><br>";
+echo "Mengintegrasi PHP dengan CSS Dalam Mengatur dan Mendesain Tabel<br><br>";
+echo "<table class = tabelphp>
+<tr>
+<th>Nama</th>
+<th>Kelas</th>
+</tr>
+<tr>
+<td>Rizky Ramadhani</td>
+<td>SI4A</td>
+</tr>
+<tr>
+<td>Rizky Ramadhani</td>
+<td>SI4A</td>
+</tr>
+<tr>
+<td>Rizky Ramadhani</td>
+<td>SI4A</td>
+</tr>
+<tr>
+<td>Rizky Ramadhani</td>
+<td>SI4A</td>
+</tr>
+<tr>
+<td>Rizky Ramadhani</td>
+<td>SI4A</td>
+</tr>
+<tr>
+<td>Rizky Ramadhani</td>
+<td>SI4A</td>
+</tr>
+</table>"
+
+?>
+</body>
+</html>
